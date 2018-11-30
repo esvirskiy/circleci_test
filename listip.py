e30a87c07881bbe66fe8cd47c18256aae7a803b3
Eugene Svirskiy <eugene.svirskiy@gmail.com>
2018-11-29 21:41:35 -0500

    fixing stuff

--- listip.py
+++ listip.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import requests, re
+import requests
+import re
 
 r = requests.get('http://httpbin.org/get')
 data = r.text
@@ -7,7 +8,4 @@ data = r.text
 ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', data)
 ip2 = str(ip)
 cleanIp = re.sub('\ |\[|\]|\'', '', ip2)
-#f = open( 'request.log', 'w' )
-#f.write(cleanIp)
-#f.close()
 print(cleanIp)
